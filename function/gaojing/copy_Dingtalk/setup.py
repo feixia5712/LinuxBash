1b65ebb150f51f855726b139d6c647276ab6b1b2
feixia571288 <571288304@qq.com>
2018-03-07 10:57:39 +0800

    ding_copy

---
+++ function/gaojing/copy_Dingtalk/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+# create time: 13/01/2018 20:41
+import io
+from setuptools import setup, find_packages
+
+with io.open("README.rst", encoding='utf-8') as f:
+    long_description = f.read()
+
+with io.open("requirements.txt", encoding='utf-8') as f:
+    install_requires = f.readlines()
+
+setup(
+    name="DingtalkChatbot",
+    version="1.1.3",
+    packages=find_packages(),
+    description="DingtalkChatbot is a Python wrapper tool for dingtalk custom chatbot messages.",
+    long_description=long_description,
+    author="Devin Zhang",
+    author_email="1324556701@qq.com",
+    url="https://github.com/zhuifengshen/DingtalkChatbot",
+    license='MIT',
+    keywords='dingtalk dingding chatbot robot bot',
+    install_requires=install_requires,
+    setup_requires=['setuptools'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ]
+)
