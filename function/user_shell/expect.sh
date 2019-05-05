9102c790c18fb80e48c3f43b38ef6fc318497415
feixia5712 <571288304@qq.com>
2019-05-05 21:47:39 +0800

    Update expect.sh

--- function/user_shell/expect.sh
+++ function/user_shell/expect.sh
@@ -1,1 +1,16 @@
+第一种直接发送命令
+EXPECT="ssh -p22 root@1.1.1.1"  能够实现免密登录
+expect <<EOF|tee /tmp/xx.log
+set timeout 1s
+spawn ${EXPECT}
+expect {
+       ">"{send "ping 1.1.1.1\r";}
+        }
+expect {
+       "" {send "quit\r";}
+expect eof
 
+EOF
+       
+       
+       
